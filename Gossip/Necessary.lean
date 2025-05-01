d80f464fa4764233f63bb6e07e3bbc6b296fc452
Malvin Gattinger <malvin@w4eg.de>
2025-05-01 13:15:08 +0200

    high-level setup

--- Gossip/Necessary.lean
+++ Gossip/Necessary.lean
@@ -68,6 +68,23 @@ def is_f n k :=
   ∧ σ.length = k
   ∧ ¬ ∃ σ' : List (Call n), allExpert (after σ') ∧ σ'.length < k
 
+/-- Nobody hears their own sequence. -/
+def noHo (σ : List (Call n)) : Prop :=
+  ∀ i : Fin σ.length,
+      ¬ (after (σ.take (i - 1))) σ[i].fst σ[i].snd
+    ∧ ¬ (after (σ.take (i - 1))) σ[i].snd σ[i].fst
+
+/-- Given a sequence of length 2n-4 or less that makes all agents experts,
+before each of the calls of the sequence, the agents in that call do
+not know each other's secrets. -/
+lemma noHo_of_minimal_expert_sequence (σ : List (Call n))
+    (h : σ.length ≤ 2 * n - 5)
+    (hExp : allExpert (after σ))
+    -- FIXME: needs extra assumptions about minimality
+    : noHo σ := by
+  -- rearranging argument
+  sorry
+
 theorem bla : ∀ n, is_f n (2*n -4) := by
   by_contra hyp
   rw [not_forall] at hyp
@@ -89,26 +106,16 @@ theorem bla : ∀ n, is_f n (2*n -4) := by
       -- have := hx _ h_eq
       sorry
 
-
-/-- Given a sequence of length 2n-4 or less that makes all agents experts,
-before each of the calls of the sequence, the agents in that call do
-not know each other's secrets. -/
-lemma noSelfHearing (σ : List (Call n))
-    (h : σ.length ≤ 2 * n - 5)
-    (hExp : allExpert (after σ))
-    : ∀ i : Fin σ.length,
-          ¬ (after (σ.take (i - 1))) σ[i].fst σ[i].snd
-        ∧ ¬ (after (σ.take (i - 1))) σ[i].snd σ[i].fst := by
-  sorry
-
+/-
 /-- For n ≥ 4 agents, any sequence of 2n-5 or fewer calls cannot make everyone experts. -/
 theorem low_not_sufficient (h : n ≥ 4) (σ : List (Call n))
     (hLen : σ.length ≤ 2*n - 5)
     : ¬ allExpert (after σ) := by
   have no_hears_own := noSelfHearing σ hLen
   sorry
+-/
 
-theorem helper (h : n ≥ 4) : k ≤ 2 * n - 5 ↔ k < 2 * n - 4 := by
+theorem helper {k n} (h : 4 ≤ n) : k ≤ 2 * n - 5 ↔ k < 2 * n - 4 := by
   rw [Nat.two_mul]
   have := @Nat.add_one_le_iff (k + 4) (n + n)
   rw [add_assoc] at this
@@ -119,12 +126,42 @@ theorem helper (h : n ≥ 4) : k ≤ 2 * n - 5 ↔ k < 2 * n - 4 := by
   exact Iff.symm Nat.lt_sub_iff_add_lt
 
 /-- For n ≥ 4 agents, any sequence that makes everyone an expert, has length 2n-4 or higher. -/
-theorem necessity (h : n ≥ 4) (σ : List (Call n))
-    (hExp : allExpert (after σ))
-    : σ.length ≥ 2*n - 4 := by
+theorem necessity :
+    ∀ n ≥ 4, ∀ σ : List (Call n), allExpert (after σ) → σ.length ≥ 2*n - 4 := by
   by_contra hyp
-  absurd hExp
-  apply low_not_sufficient h σ
-  simp only [ge_iff_le, not_le] at hyp
-  rw [helper h]
-  assumption
+  simp only [ge_iff_le, not_forall, not_le] at hyp
+  -- let φ n := ∃ (_ : 4 ≤ n), ∃ σ : List (Call n), ∃ (h : allExpert (after σ)), σ.length < 2 * n - 4
+  have := exists_to_minimal_exists _ hyp
+  rcases this with ⟨m, ⟨le_m, ⟨S, S_allExp, S_len_lt⟩⟩, m_is_minimal⟩
+
+  rw [← helper le_m] at S_len_lt
+
+  have noHo_S : noHo S := by
+    apply noHo_of_minimal_expert_sequence
+    -- TODO ;-)
+    all_goals assumption
+
+  -- TODO: define final and initial first
+  -- have claim1 : sorry := sorry -- "all calls in S are final for both or neither"
+  -- have claim2 : sorry := sorry -- "all calls in S are initial for both or neither"
+
+  -- TODO: define graph
+
+  let initPart : List (Call m) := sorry
+  let midPart : List (Call m) := sorry
+  let finalPart : List (Call m) := sorry
+
+  have : S.length = initPart.length + midPart.length + finalPart.length := by
+    sorry
+
+  have : m / 2 = initPart.length := by sorry
+
+  have : m / 2 = finalPart.length := by sorry
+
+  have : m = initPart.length + finalPart.length := by sorry -- division problem?
+
+  have len_s_at_least : m ≤ S.length := by linarith
+
+  have : midPart.length ≤ m - 5 := by sorry -- or was it flipped around?
+
+  sorry
