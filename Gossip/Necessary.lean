f14abced230977da09fed97bddf0146bdac6229f
andreiM <andreimarculesteanu28@gmail.com>
2025-05-09 13:03:38 +0200

    work on NoHo_S under the necessity assumptions

--- Gossip/Necessary.lean
+++ Gossip/Necessary.lean
@@ -82,53 +82,21 @@ def noHo (σ : List (Call n)) : Prop :=
 def phi (n : Nat) : Prop :=
   n > 4 ∧ is_f_leq n (2*n - 5)
 
-def min_m :=
-  ∃ m : Nat, phi m
+def is_minimal (m: Nat) :=
+  phi m
   ∧ ∀ n > m, ¬ phi n
 
 /-- Given a sequence of length 2n-4 or less that makes all agents experts,
 before each of the calls of the sequence, the agents in that call do
 not know each other's secrets. -/
-lemma noHo_of_minimal_expert_sequence (σ : List (Call n))
-    (h : σ.length ≤ 2 * n - 5)
+lemma noHo_of_minimal_expert_sequence (σ : List (Call m))
+    (h : σ.length ≤ 2 * m - 5)
     (hExp : allExpert (after σ))
-    (h_min : min_m)
-    -- FIXME: needs extra assumptions about minimality
+    (h_min : is_minimal m)
     : noHo σ := by
-    -- have : exists_to_minimal_exists phi
+    obtain ⟨h_phi, h_minimal⟩ := h_min
   -- rearranging argument
     sorry
-
-/--/
-theorem bla : ∀ n, is_f n (2*n -4) := by
-  by_contra hyp
-  rw [not_forall] at hyp
-  have := exists_to_minimal_exists _ hyp
-  simp at this
-  rcases hyp with ⟨x, hx⟩
-  rcases this with ⟨m, hm, h_min⟩
-  by_cases h : x < m
-  case pos =>
-    have := h_min _ h
-    exact hx this
-  case neg =>
-    simp at h
-    rcases lt_or_eq_of_le h with h_less | h_eq
-    case inl =>
-      -- have := h_min _ h_less
-      sorry
-    case inr =>
-      -- have := hx _ h_eq
-      sorry
--/
-/-
-/-- For n ≥ 4 agents, any sequence of 2n-5 or fewer calls cannot make everyone experts. -/
-theorem low_not_sufficient (h : n ≥ 4) (σ : List (Call n))
-    (hLen : σ.length ≤ 2*n - 5)
-    : ¬ allExpert (after σ) := by
-  have no_hears_own := noSelfHearing σ hLen
-  sorry
--/
 
 theorem helper {k n} (h : 4 ≤ n) : k ≤ 2 * n - 5 ↔ k < 2 * n - 4 := by
   rw [Nat.two_mul]
@@ -142,19 +110,42 @@ theorem helper {k n} (h : 4 ≤ n) : k ≤ 2 * n - 5 ↔ k < 2 * n - 4 := by
 
 /-- For n ≥ 4 agents, any sequence that makes everyone an expert, has length 2n-4 or higher. -/
 theorem necessity :
-    ∀ n ≥ 4, ∀ σ : List (Call n), allExpert (after σ) → σ.length ≥ 2*n - 4 := by
+    ∀ n > 4, ∀ σ : List (Call n), allExpert (after σ) → σ.length ≥ 2*n - 4 := by
   by_contra hyp
   simp only [ge_iff_le, not_forall, not_le] at hyp
   -- let φ n := ∃ (_ : 4 ≤ n), ∃ σ : List (Call n), ∃ (h : allExpert (after σ)), σ.length < 2 * n - 4
   have := exists_to_minimal_exists _ hyp
   rcases this with ⟨m, ⟨le_m, ⟨S, S_allExp, S_len_lt⟩⟩, m_is_minimal⟩
 
-  rw [← helper le_m] at S_len_lt
+  -- rw [← helper le_m] at S_len_lt
 
   have noHo_S : noHo S := by
     apply noHo_of_minimal_expert_sequence
     -- TODO ;-)
-    all_goals assumption
+    · rw [@Nat.le_iff_lt_add_one]
+      omega
+    · assumption
+    · unfold is_minimal
+      unfold phi
+      constructor
+      · constructor
+        · assumption
+        · unfold is_f_leq
+          use S
+          constructor
+          · exact S_allExp
+          · rw [@Nat.le_iff_lt_add_one]
+            omega
+      · intro n n_gtm
+        rw [Mathlib.Tactic.PushNeg.not_and_or_eq]
+        right
+        unfold is_f_leq
+        push_neg
+        intro σ σ_AllExp
+        simp at m_is_minimal
+        -- Use m_is_minimal
+        sorry
+
 
   -- TODO: define final and initial first
   -- have claim1 : sorry := sorry -- "all calls in S are final for both or neither"
